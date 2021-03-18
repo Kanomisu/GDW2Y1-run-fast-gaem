6ad87f87e46d6b0bf45d462eb10f690671cc9e8e
JonahReeves <jonahareeves@gmail.com>
2021-03-18 06:31:21 -0400

    Rope Prog
    Rope basically done, just a bug when deleting the hook crashing the game to fix. Pushed in case any of yall wanna try to fix before I woke

--- SpriteLib3.0-v2.0/SpriteLib3.0-v2.0/PhysicsPlayground.h
+++ SpriteLib3.0-v2.0/SpriteLib3.0-v2.0/PhysicsPlayground.h
@@ -29,6 +29,8 @@ public:
 	void KeyboardUp() override;
 
 	int ShootHook();
+	int ShootRope();
+	void UpdateRope();
 	
 	void SpawnEnemy(float spawnX, float spawnY, float roamA, float roamB) {
 		std::cout << "Enemy Spawned" << std::endl;
@@ -100,11 +102,17 @@ public:
 	{
 		if (m_hookDeleteQueued)
 		{
+			if (activeRope != NULL)
+			{
+				PhysicsBody::m_bodiesToDelete.push_back(activeRope);
+				activeRope = NULL;
+			}
 			if (activeHook != NULL)
 			{
 				PhysicsBody::m_bodiesToDelete.push_back(activeHook);
 				activeHook = NULL;
 			}
+			
 			m_hookDeleteQueued = false;
 		}
 	}
@@ -159,6 +167,7 @@ protected:
 
 	int playerRef;
 	int activeHook = NULL;
+	int activeRope = NULL;
 
 	vec2 m_mousePos = vec2(0.f, 0.f);
 
